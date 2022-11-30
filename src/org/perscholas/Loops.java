8035c47e7aa7d3c175b2be846202f6e8647f3c70
Edward Barcenas <edward.barcenas16@gmail.com>
2022-11-30 11:02:39 -0600

    changed format for questions

--- src/org/perscholas/Loops.java
+++ src/org/perscholas/Loops.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class Loops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		multiplicationTable();
-		greatestCommonDivisor(sc);
-		futureTuition();
+		question1();
+		question2(sc);
+		question3();
 		sc.close();
 	}
 	
 	// Question 1 Write a program that uses nested for loops to print a multiplication table.
-	public static void multiplicationTable() {
+	public static void question1() {
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 12; j++) {
 				System.out.print((i+1) * (j+1) + " ");
@@ -25,7 +24,7 @@ public class Loops {
 	}
 	
 	// Question 2 Write a program that prompts the user to enter two positive integers, and find their greatest common divisor (GCD).
-	public static void greatestCommonDivisor(Scanner sc) {
+	public static void question2(Scanner sc) {
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		
@@ -42,7 +41,7 @@ public class Loops {
 	 Suppose the tuition for a university is $10,000 for the current year and increases by 7 percent every year. 
 	 In how many years will the tuition be doubled?
 	*/
-	public static void futureTuition() {
+	public static void question3() {
 		double tuition = 10000;
 		int years = 0;
 		while (true) {
